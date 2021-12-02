1d384ef0cee8a888cecfe6eed99c60ea018ab3c2
tinierZhao <tinierZhao@qq.com>
2021-12-02 11:19:19 +0800

    push commit

--- src/main/java/com/tinierZhao/git/Gitclass.java
+++ src/main/java/com/tinierZhao/git/Gitclass.java
@@ -8,5 +8,6 @@ public class Gitclass {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hot - fix text");
+        System.out.println("push   text");
     }
 }
